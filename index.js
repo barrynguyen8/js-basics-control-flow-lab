147863b7504ff1fcb6f2dfc93432fa322ce7c9b6
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-03-07 01:45:28 +1100

    assignment submitted

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  } else if (feet > 2500) {
+      return 'No can do.'
+  } else if (feet > 2000) {
+      return 'I will gladly take your thirty bucks.' 
+}
+scuberGreetingForFeet(199);
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') { 
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
+
+
+
